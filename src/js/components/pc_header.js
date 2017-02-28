4964f55932b30522c0f73649177a84a6eb3f4c2a
jinjianhua <805001458@qq.com>
2017-02-28 21:25:06 +0800

    添加登录注册退出功能

--- src/js/components/pc_header.js
+++ src/js/components/pc_header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Row, Col } from 'antd';
-import { Menu, Icon ,Tabs,Message,Form,Input,Button,CheckBox,Modal} from 'antd';
-
+import { Menu, Icon ,Tabs,message,Form,Input,Button,CheckBox,Modal} from 'antd';
+import {Router, Route, Link, browserHistory} from 'react-router'
 const FormItem =Form.Item;
 const SubMenu =Menu.SubMenu;
 const TabPane = Tabs.TabPane;
@@ -18,6 +18,12 @@ class PCHeader extends React.Component{
 			userid:0
 		}
 	}
+	componentWillMount(){
+		if (localStorage.userid!='') {
+			this.setState({hasLogined:true});
+			this.setState({userNickName:localStorage.userNickName,userid:localStorage.userid});
+		}
+	};
 	setModalVisible(value) {
 		this.setState({
 			modalVisible:value
@@ -36,7 +42,45 @@ class PCHeader extends React.Component{
 		}
 	}
 	handleSubmit(e) {
+		//页面开始向 API 进行提交数据
+		e.preventDefault();
+		var myFetchOptions = {
+			method: 'GET'
+		};
+		var formData = this.props.form.getFieldsValue();
+		console.log(formData);
+		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=" + this.state.action
+		+ "&username="+formData.userName+"&password="+formData.password
+		+"&r_userName=" + formData.r_userName + "&r_password="
+		+ formData.r_password + "&r_confirmPassword="
+		+ formData.r_confirmPassword, 
+		myFetchOptions)
+		.then(response => response.json())
+		.then(json => {
+			this.setState({userNickName: json.NickUserName, userid: json.UserId});
+			localStorage.userid= json.UserId;
+			localStorage.userNickName = json.NickUserName;
+		});
+		if (this.state.action==='login') {
+			this.setState({
+				hasLogined:true
+			})
+		}
 
+		message.success("请求成功！");
+		this.setModalVisible(false);
+	}
+	callback(key){
+		if (key == 1) {
+			this.setState({action: 'login'});
+		} else if (key == 2) {
+			this.setState({action: 'register'});
+		}
+	}
+	logout(){
+		localStorage.userid= '';
+		localStorage.userNickName = '';
+		this.setState({hasLogined:false});
 	}
 	render(){
 		let {getFieldProps} = this.props.form;
@@ -48,7 +92,7 @@ class PCHeader extends React.Component{
 				<Button type="dashed" htmlType='button'>个人中心</Button>
 			</Link>
 			&nbsp;&nbsp;
-			<Button type="ghost" htmlType='button'>退出</Button>
+			<Button type="ghost" htmlType='button' onClick={this.logout.bind(this)}>退出</Button>
 		</Menu.Item>
 		: <Menu.Item key='register' class='register'>
 			<Icon type="appstore"/>注册/登录
@@ -95,9 +139,21 @@ class PCHeader extends React.Component{
 					</Menu>
 
 					<Modal title="用户中心" wrapClassName="verical-center-modal" visible={this.state.modalVisible} onCancle={()=>this.setModalVisible(false)} onOk={()=> this.setModalVisible(false)} okText="关闭">
-						<Tabs type="card">
+						<Tabs type="card" onChange={this.callback.bind(this)}>
+							<TabPane tab="登录" key="1">
+								<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+									<FormItem label="账户">
+										<Input placeholder="请输入您的帐号" {...getFieldProps('userName')}/>
+									</FormItem>
+									<FormItem label="密码">
+										<Input type="password" placeholder="请输入您的密码" {...getFieldProps('password')}/>
+									</FormItem>
+									<Button type="primary" htmlType="submit">登录</Button>
+								</Form>
+							</TabPane>
+
 							<TabPane tab="注册" key="2">
-								<Form forizontal onSubmit={this.handleSubmit.bind(this)}>
+								<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
 									<FormItem label="账户">
 										<Input placeholder="请输入您的帐号" {...getFieldProps('r_userName')}/>
 									</FormItem>
@@ -105,7 +161,7 @@ class PCHeader extends React.Component{
 										<Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
 									</FormItem>
 									<FormItem label="账户">
-										<Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirm')}/>
+										<Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
 									</FormItem>
 									<Button type="primary" htmlType="submit">注册</Button>
 								</Form>
