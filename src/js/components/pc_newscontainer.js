fbeeef2536f62171c4203431c68f6c5d64dd67fa
jinjianhua <805001458@qq.com>
2017-03-02 08:17:12 +0800

    个人中心模块

--- src/js/components/pc_newscontainer.js
+++ src/js/components/pc_newscontainer.js
@@ -3,6 +3,7 @@ import {Row, Col} from 'antd';
 import {Tabs, Carousel} from 'antd';
 import PCNewsBlock from './pc_news_block';
 import PCNewsImageBlock from './pc_news_image_block';
+import PCProduct from './pc_products';
 const TabPane = Tabs.TabPane;
 
 export default class PCNewsContainer extends React.Component {
@@ -36,6 +37,11 @@ export default class PCNewsContainer extends React.Component {
 							</TabPane>
 							<TabPane tab="国际" key="2">
 								<PCNewsBlock count={22} type="guoji" width="100%" bordered="false"/>
+							</TabPane>
+						</Tabs>
+						<Tabs class="tabs_product">
+							<TabPane tab="ReactNews 产品" key="1">
+								<PCProduct/>
 							</TabPane>
 						</Tabs>
 						<div>
